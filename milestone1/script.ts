e4a48c635239a24942ad4482e64045b5645f4e42
zubaida <165196442+qainataries@users.noreply.github.com>
2024-09-17 11:05:04 +0500

    Add files via upload

---
+++ milestone1/script.ts
@@ -0,0 +1,10 @@
+ let toggleButton= document.getElementById('toggle-skills') as HTMLButtonElement
+ let  skills = document.getElementById('skills') as HTMLElement
+toggleButton.addEventListener('click' ,()=>{
+    if(skills.style.display==='none'){
+        skills.style.display='block'
+
+    }else{
+        skills.style.display='none'
+    }
+});
